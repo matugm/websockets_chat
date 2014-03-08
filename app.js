05a3f47e8589b4a8a75b77c525c3cd4ad3c2f7e4
matugm <matugm@gmail.com>
2014-03-08 18:06:38 +0100

    add basic admin features (ban/unban)

--- app.js
+++ app.js
@@ -25,16 +25,18 @@ function genRandomUserName() {
 }
 
 user_count = 0;
+banned_users = [];
+admin_pass = "92zn4tEU";
 
 // Actual WebSockets stuff starts here
 io.sockets.on('connection', function (socket) {
   socket.emit('server_message', { msg: 'Welcome to our chat!' });
 
   user_count += 1;
-  console.log(user_count);
 
   var user = {};
-  user.name = genRandomUserName();
+  user.name  = genRandomUserName();
+  user.admin = false;
   user.msg_count = 0;
   user.last_message = new Date().getTime();
   socket.set('user_data', user);
@@ -49,10 +51,53 @@ io.sockets.on('connection', function (socket) {
     socket.broadcast.emit('new_user', { count: user_count });
   });
 
+  socket.on('admin_cmd', function(data) {
+    socket.get('user_data', function(err, user) {
+      var cmd  = data.msg.split(" ")[0];
+      var args = data.msg.split(" ")[1];
+
+      if (cmd == "/pass" && args == admin_pass) {
+        user.admin = true;
+        socket.emit('server_message', { msg: "You are now recognized as an admin."});
+        socket.set('user_data', user);
+      }
+
+      if (!user.admin) {
+        socket.emit('server_message', { msg: "Sorry, you need to be an admin to do that." });
+        return false;
+      }
+
+      if (cmd == "/ban") {
+        banned_users.push(args);
+        socket.emit('server_message', { msg: args + " has been banned from chat."});
+      }      
+
+      if (cmd == "/unban") {
+        var index = banned_users.indexOf(args);
+
+        if (index == -1) {
+          socket.emit('server_message', { msg: "User is not banned." });
+          return;
+        }
+
+        banned_users.splice(index, index + 1);
+        console.log(index);
+        socket.emit('server_message', { msg: args + " has been unbanned."});
+      }
+
+    });
+  });
+
   socket.on('send_message', function (data) {
     socket.get('user_data', function(err, user) {
       var time = new Date().getTime();
       var diff = time - user.last_message;
+
+      if (banned_users.indexOf(user.name) > -1) {
+        socket.emit('server_message', { msg: 'Sorry, you have been banned.'});
+
+        return false;
+      }
 
       if (diff < 8000) {
         if (user.msg_count >= 3) {
